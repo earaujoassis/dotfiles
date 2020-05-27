aa983f024404fcbf201b4e77dcb89a777c9d3a1b
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-05-27 14:08:22 -0300

    Add aliases for git

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
 # Aliases for ZSH
 
+ZSHDIR=$(dirname $(realpath "$0"))
+source $ZSHDIR/git.zsh
+
 SUBLIME_MACOS="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 if [ -f "$SUBLIME_MACOS" ]; then
     alias sublime = "$SUBLIME_MACOS"
---
+++ zsh/git.zsh
@@ -0,0 +1,4 @@
+# Aliases for Git at ZSH
+
+alias git-push="git push -u origin HEAD"
+alias git-pull="git fetch && git pull origin"
