3d9aad1e5631eaa97b4e189ac755cbb752c623fe
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-06-18 15:59:33 -0300

    Set the default strategy for git-pull

--- git-setup.sh
+++ git-setup.sh
@@ -7,6 +7,7 @@ echo 'Setup git'
 git config --global user.name "Carlos A."
 git config --global user.email 593456+earaujoassis@users.noreply.github.com
 git config --global core.excludesfile ~/dotfiles/.gitignore
+git config --global pull.rebase false  # merge (the default strategy)
 
 # git config --global user.signingkey ${KEY}
 git config --global commit.gpgsign true
