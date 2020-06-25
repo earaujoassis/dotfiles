954f776153e3b7712418cc25b54f7e080c53b974
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-06-25 12:14:47 -0300

    Add git-tag function

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -3,3 +3,12 @@
 alias git-push="git push -u origin HEAD"
 alias git-pull="git fetch && git pull origin"
 alias git-master="git checkout master"
+
+git-tag() {
+    if [ "$1" ]
+    then
+        git tag $1 && git push origin $1
+    else
+        echo "Error: missing tag name"
+    fi
+}
