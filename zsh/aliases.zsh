e95cb5ef9c99d5fbccaf2f40407cd0b7c98d41ee
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-07-17 14:54:05 -0300

    feature: add new aliases; fix: giffy

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,11 @@ SUBLIME_MACOS="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 if [ -f "$SUBLIME_MACOS" ]; then
     alias sublime = "$SUBLIME_MACOS"
 fi
+
+ps-sbt() {
+    ps aux | grep sbt
+}
+
+kill-sbt() {
+    kill -9 $(ps aux | grep sbt | awk '{ print $2 }' | sed -n 1p)
+}
