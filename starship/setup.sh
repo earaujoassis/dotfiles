de340d1bec8dc4575732a59d16820d0d75f47649
Carlos A <593456+earaujoassis@users.noreply.github.com>
2022-07-28 20:40:11 -0300

    chore: add starship configuration file

--- starship/setup.sh
+++ starship/setup.sh
@@ -13,6 +13,8 @@ then
     curl -fsSL https://starship.rs/install.sh | sh
     echo "" >> $HOME_PROFILE
     echo 'eval "$(starship init zsh)"' >> $HOME_PROFILE
+    mkdir -p $HOME/.config
+    ln -s $BASEDIR/starship.toml $HOME/.config/starship.toml
 fi
 
 echo '> Done!'
