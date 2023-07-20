478a19a742615f0a74a92a02fffa196dbfc9d35d
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-07-20 18:53:55 -0300

    hotfix: prezto install

--- prezto/setup.sh
+++ prezto/setup.sh
@@ -11,7 +11,7 @@ then
 
     setopt EXTENDED_GLOB
     for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     done
 
     echo '> Done!'
