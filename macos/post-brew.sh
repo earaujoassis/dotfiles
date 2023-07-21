774643ae6893c498f1b4e86a6e4dfcef87c9b08b
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-07-21 12:21:06 -0300

    hotfix: post-brew installation fixes

--- macos/post-brew.sh
+++ macos/post-brew.sh
@@ -4,10 +4,21 @@ set -e
 
 echo '> Post-brew install'
 
+echo '>> Maccy configuration'
 # === Maccy ===
 # https://github.com/p0deje/Maccy
 defaults write org.p0deje.Maccy pasteByDefault true
 defaults write org.p0deje.Maccy historySize 20
 defaults write org.p0deje.Maccy ignoredPasteboardTypes -array-add "com.myapp.CustomType"
+
+HOME_PROFILE=$HOME/.profile
+touch $HOME_PROFILE
+
+if ! grep -q 'ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR' "$HOME_PROFILE"; then
+    echo '>> zsh highlight configuration'
+    echo "" >> $HOME_PROFILE
+    echo 'export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters' >> $HOME_PROFILE
+    echo 'source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> $HOME_PROFILE
+fi
 
 echo '> Done!'
