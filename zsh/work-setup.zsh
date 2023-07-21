cc255b3bb710f25a21d54d13844fa55735642ada
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-07-21 17:49:26 -0300

    chore: setup work and personal profiles for git

---
+++ zsh/work-setup.zsh
@@ -0,0 +1,31 @@
+# Aliases for Git at ZSH (Work related)
+
+git-work-profile() {
+    ENV_FILE=$HOME/dotfiles/config/.env
+    if grep -q 'GIT_WORK_EMAIL' "$ENV_FILE" && [ -f "$ENV_FILE" ]; then
+        set -o allexport
+        source $ENV_FILE
+        git config --global user.email "$GIT_WORK_EMAIL"
+        set +o allexport
+        echo "> Using the following email now: $(git config --global user.email)"
+    elif ! [ -f "$ENV_FILE" ]; then
+        echo 'Error: missing $HOME/dotfiles/config/.env file'
+    else
+        echo 'Error: missing GIT_WORK_EMAIL in $HOME/dotfiles/config/.env file'
+    fi
+}
+
+git-personal-profile() {
+    ENV_FILE=$HOME/dotfiles/config/.env
+    if grep -q 'GIT_PERSONAL_EMAIL' "$ENV_FILE" && [ -f "$ENV_FILE" ]; then
+        set -o allexport
+        source $ENV_FILE
+        git config --global user.email "$GIT_PERSONAL_EMAIL"
+        set +o allexport
+        echo "> Using the following email now: $(git config --global user.email)"
+    elif ! [ -f "$ENV_FILE" ]; then
+        echo 'Error: missing $HOME/dotfiles/config/.env file'
+    else
+        echo 'Error: missing GIT_PERSONAL_EMAIL in $HOME/dotfiles/config/.env file'
+    fi
+}
