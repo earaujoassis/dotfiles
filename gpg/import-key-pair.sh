49ea7615b6351cb7972e426c08b1a49ce67b537f
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-07-21 12:11:19 -0300

    hotfix: gpg key import

--- gpg/import-key-pair.sh
+++ gpg/import-key-pair.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo '> Enter secret-key path to import: '
-read $secret_path
-
-gpg --output - $secret_path | gpg --import
-
 echo '> Enter public-key path to import: '
-read $public_path
+read public_path
 
 gpg --import $public_path
+
+echo '> Enter secret-key path to import: '
+read secret_path
+
+gpg --output - "$secret_path" | gpg --import
 
 echo '> Done!'
