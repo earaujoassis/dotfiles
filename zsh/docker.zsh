4dca8be6fd7cb3b49b342a52db2c7cbeffe59753
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-08-29 18:49:39 -0300

    chore: expand docker aliases

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -3,3 +3,21 @@
 docker-bash() {
     docker exec -i -t $1 sh
 }
+
+docker-stop-all() {
+    if [ "$(docker ps -a -q)" ]; then
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+    else
+        echo "> No running containers; skipping"
+    fi
+}
+
+docker-prune() {
+    docker system prune -a
+    if [ "$(docker volume ls -q)" ]; then
+        docker volume rm $(docker volume ls -q)
+    else
+        echo "> No available volumes; skipping"
+    fi
+}
